f07587e29bdf8bf147a892195e95d6340c8c4209
Nawfal Ahmed <nawfal.ahmed@arbisoft.com>
2022-10-11 14:15:56 +0500

    Update User/Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,47 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+     @user = User.new(user_params)
+
+     if @user.save
+        redirect_to :action => 'index'
+     else
+        render :action => 'new'
+     end
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+     @user = User.find(params[:id])
+
+     if @user.update_attributes(user_params)
+        redirect_to :action => 'show', :id => @user
+     else
+        render :action => 'edit'
+     end
+
+  end
+
+  def delete
+    User.find(params[:id]).destroy
+    redirect_to :action => 'index'
+  end
+
+  def user_params
+     params.require(:users).permit(:name, :roll_no)
+  end
 end
